03e2be74074da1ccc58cf0d9c8b9c90c20d48d0f
Ignacio Quiroga <ignacioequiroga@gmail.com>
2020-04-19 21:58:41 -0300

    Subiendo el punto 4

--- routes/heroesDetalleConBio.js
+++ routes/heroesDetalleConBio.js
@@ -4,24 +4,43 @@ const fs = require('fs');
 
 const heroes = JSON.parse(fs.readFileSync('./data/heroes.json'));
 
-router.get('/heroes/bio/:id/:ok?', (req, res) =>{
-    let idHeroe = req.params.id-1;
-    
-    switch (idHeroe) {
-        case idHeroe > heroes.length :
-            let textoRespuesta = `No encontramos un héroe para mostrarte su biografía`;
-            res.send(textoRespuesta);
-            break;
-        
-        default:
-            break;
-    }
+router.get('/heroes/bio/:id/:ok?', (req, res) => {
+    let idHeroe = req.params.id - 1;
+    let okUrl = req.params.ok;
+    let printHeroe = heroes[idHeroe];
 
-    /*if(idHeroe > heroes.length){
+    if (idHeroe > heroes.length && (okUrl == 'ok' || okUrl != 'ok')) {
         let textoRespuesta = `No encontramos un héroe para mostrarte su biografía`;
         res.send(textoRespuesta);
+    } else {
+        if (okUrl != 'ok') {
+            let textoRespuesta = `Mi nombre es ${printHeroe.nombre}. Lamento que no desees saber más de mí :(`;
+            res.send(textoRespuesta);
+        } else {
+            if ((idHeroe < heroes.length) && (okUrl == 'ok')) {
+                let = textoRespuesta = `${printHeroe.resenia}`;
+                res.send(textoRespuesta);
+            }
+        }
+    }
+    /*switch (idHeroe, okUrl) {
+        case idHeroe < heroes.length:
+            let respuesta1 = `${printHeroe.resenia}`
+            res.send(respuesta1);
+        break;
+        case okUrl != 'ok':
+            let respuesta2 = `Mi nombre es ${printHeroe.nombre}. Lamento que no desees saber más de mí :(`;
+            res.send(respuesta2);
+        break;
+
+        default:
+            let respuesta3 = `${printHeroe.resenia}`;
+            res.send(respuesta3);
+            console.log(heroes.length);
+            console.log(idHeroe);
+            console.log(okUrl);
+            
     };*/
-    
 });
 
 module.exports = router;
